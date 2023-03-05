dcdb8ac77221df81cf0b1f3f68908ccdda6e3dc4
Jose Pascuas <josemi0@hotmail.com>
2023-03-05 02:31:56 -0500

    ejercicio 2 js objetos y listas

---
+++ mi-proyecto-node/ejercicio2.js
@@ -0,0 +1,12 @@
+const listaPersonal= [
+    "Jose Miguel",
+    24, 
+    true,
+    new Date(1999, 0, 29),
+    {
+        titulo: "El nombre del viento",
+        autor: "Patrick Rothfuss",
+        fecha: 2007,
+        url: "https://es.wikipedia.org/wiki/El_nombre_del_viento"
+    }
+]
