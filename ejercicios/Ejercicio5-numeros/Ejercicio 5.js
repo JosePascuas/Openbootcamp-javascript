fa8d5826343b14ad1ff71b58c17051e1096a2aff
Jose Pascuas <josemi0@hotmail.com>
2023-03-07 18:08:34 -0500

    desarrollo ejercicio 5 JS

---
+++ ejercicios/Ejercicio5-numeros/Ejercicio 5.js
@@ -0,0 +1,12 @@
+let alturaCm= 176
+let alturaMetros= alturaCm /100 
+let pesoKg= 78.5
+
+let alturaMetrosRedondeada= Math.ceil(alturaMetros)
+console.log(alturaMetrosRedondeada)
+
+let pesoKgRedondeado= Math.floor(pesoKg)
+console.log(pesoKgRedondeado);
+
+let MaxValor= (Number.MAX_VALUE+1) === Number.MAX_VALUE
+console.log(MaxValor)
