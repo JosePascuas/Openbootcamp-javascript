6fb443f52b95a35f30199e6e72a7944520f65cc2
Jose Pascuas <josemi0@hotmail.com>
2023-03-31 16:10:33 -0500

    ejercicio eslint

---
+++ ejercicios/ejercicio-14/index.js
@@ -0,0 +1,25 @@
+let nombre= "Jose miguel";
+let apellido= "Pascuas carvajal";
+let estudiante= nombre+" "+apellido
+console.log(estudiante)
+
+let estudianteMayus= estudiante.toUpperCase();
+console.log(estudianteMayus)
+
+let estudianteMinus= estudiante.toLowerCase();
+
+let numeros_palabras= estudiante.length;
+console.log(numeros_palabras)
+
+let primera_letra= nombre.charAt();
+console.log(primera_letra)
+
+let primer_letra_apellido= apellido.charAt();
+console.log(primer_letra_apellido)
+
+
+let eliminar_espacios= estudiante.trim().length
+console.log(eliminar_espacios)
+
+let se_encuentra= estudiante.includes("Jose");
+console.log(se_encuentra)
