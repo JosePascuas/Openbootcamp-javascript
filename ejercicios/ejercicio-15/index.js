67d6c73d722df8f05d27bae88b83ebc7ff483dec
Jose Pascuas <josemi0@hotmail.com>
2023-04-12 19:15:40 -0500

    ejercicio 15

---
+++ ejercicios/ejercicio-15/index.js
@@ -0,0 +1,11 @@
+let boton= document.getElementById("boton")
+
+
+boton.addEventListener("click", ()=>{
+  alert("se hizo click")
+})
+
+$("#boton").click(()=>{
+  console.log("hola, estoy utilizando jQuery")
+})
+
