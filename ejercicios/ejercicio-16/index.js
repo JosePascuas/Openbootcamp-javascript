e09b26f8edf1566d43531cf4efc2b61955547a32
Jose Pascuas <josemi0@hotmail.com>
2023-04-12 20:44:32 -0500

    ejercicio 16

---
+++ ejercicios/ejercicio-16/index.js
@@ -0,0 +1,12 @@
+let nombre = "Jose Miguel"
+let apellido= "Pascuas Carvajal"
+
+const obj= {
+  nombre,
+  apellido
+}
+
+// localStorage.setItem("persona", JSON.stringify(obj))
+// sessionStorage.setItem("persona", JSON.stringify(obj))
+
+// document.cookie=`persona=${JSON.stringify(obj)}; expires=` + new Date(Date.now() + 2 * 60 * 1000).toUTCStringS
